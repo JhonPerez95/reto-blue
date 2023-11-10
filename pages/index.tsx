e4184eb61d3d07ddef5df7a35088bbd6938b0658
Jhon Perez <jhonperez.j95@gmail.com>
2023-11-09 20:50:41 -0500

    add first api pokemon list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,44 @@
-import { NextPage } from 'next'
+import { NextPage, GetStaticProps } from 'next'
 import { Button } from '@nextui-org/react'
 import { Layout } from '../components/layouts/'
-
-const HomePage: NextPage = () => {
+import { IPokemonListResponse, ISmallPokemon } from '../interfaces/'
+import { pokeApi } from '../api'
+interface Props {
+  pokemons: ISmallPokemon[]
+}
+const HomePage: NextPage<Props> = ({ pokemons }) => {
   return (
     <div>
       <Layout title="Blue Express" />
+      {pokemons.map((pokemon) => (
+        <>
+          <div key={pokemon.id}>
+            <p>{pokemon.name}</p>
+            <img src={pokemon.img} alt={pokemon.name} />
+          </div>
+        </>
+      ))}
       <Button color="primary">Click me</Button>
     </div>
   )
 }
 
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await pokeApi.get<IPokemonListResponse>('/pokemon?limit=151')
+
+  const pokemons: ISmallPokemon[] = data.results.map((poke, i) => ({
+    ...poke,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+      i + 1
+    }.svg`,
+  }))
+
+  return {
+    props: {
+      title: 'Blue Express',
+      pokemons,
+    },
+  }
+}
 export default HomePage
