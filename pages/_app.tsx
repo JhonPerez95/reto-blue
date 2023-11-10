0f26b61cb37a49040ade00632d161c68cca61804
Jhon Perez <jhonperez.j95@gmail.com>
2023-11-09 23:30:23 -0500

    add icon Blue Express

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,17 @@ import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react'
 
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="https://play-lh.googleusercontent.com/rF0e0IWIk7Fx_To-PnBLlAZtS9v14Bt5OdiF_kQ9thEvm6DUrUHXTzpIt6lPt7IlfcM=w240-h480-rw"
+        />
+      </Head>
       <main className="dark">
         <Component {...pageProps} />
       </main>
